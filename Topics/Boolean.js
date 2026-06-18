d8177be31c80748a04b140fe96869cd09a909655
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-19 00:40:08 +0530

    Boolean Concept

---
+++ Topics/Boolean.js
@@ -0,0 +1,18 @@
+
+        // console.log(5>8);
+        // console.log(typeof (6>9));
+        // console.log(typeof true);
+        // console.log(5== 5);
+        // console.log(5==4);
+        // console.log(5===5);
+        // console.log(5===4);
+        if(21>18){
+            console.log("u can drive car");
+        }
+        else if(18>9){
+            console.log("u no drive car");
+        }else{
+             console.log("no");
+        }
+
+    
