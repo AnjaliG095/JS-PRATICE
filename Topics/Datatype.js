66a467d1730efc501f00f3b9b71a24bc4b983e60
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-28 22:47:38 +0530

    Datatypes in JS

---
+++ Topics/Datatype.js
@@ -0,0 +1,48 @@
+
+    let name = "Anjali";
+    console.log(name);
+    console.log(typeof name);
+
+    // Number
+    let age = 20;
+    console.log(age);
+    console.log(typeof age);
+
+    // Boolean
+    let isStudent = true;
+    console.log(isStudent);
+    console.log(typeof isStudent);
+
+    // Undefined
+    let city;
+    console.log(city);
+    console.log(typeof city);
+
+    // Null
+    let marks = null;
+    console.log(marks);
+    console.log(typeof marks);
+
+    // BigInt
+    let bigNumber = 12345678901234567890n;
+    console.log(bigNumber);
+    console.log(typeof bigNumber);
+
+    // Symbol
+    let id = Symbol("id");
+    console.log(id);
+    console.log(typeof id);
+
+    // Object
+    let student = {
+        name: "Anjali",
+        age: 20
+    };
+    console.log(student);
+    console.log(typeof student);
+
+    // Array
+    let colors = ["Red", "Blue", "Green"];
+    console.log(colors);
+    console.log(typeof colors);
+
