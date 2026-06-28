9e4fbc557b664978771ac4080af6b0f7e164cbbd
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-28 18:15:46 +0530

    LOgical Operators

--- Topics/LogicalOperator.js
+++ Topics/LogicalOperator.js
@@ -1,4 +1,33 @@
- let age = 70;
-if(age>60 && age<80){
-    console.log("you can drive");
-}
+
+        // Logical Operators in JavaScript
+    
+        let age = 20;
+        let hasLicense = true;
+
+       
+
+        console.log("Logical AND (&&)");
+        console.log(age >= 18 && hasLicense); // true
+        console.log(age < 18 && hasLicense);  // false
+
+
+ 
+        console.log("Logical OR (||)");
+        console.log(age >= 18 || hasLicense); // true
+        console.log(age < 18 || hasLicense);  // true
+        console.log(false || false);          // false
+
+
+        console.log("Logical NOT (!)");
+        console.log(!hasLicense); // false
+        console.log(!true);       // false
+        console.log(!false);      // true
+
+
+        if (age >= 18 && hasLicense) {
+            console.log("You are allowed to drive.");
+        } else {
+            console.log("You are not allowed to drive.");
+        }
+
+    
