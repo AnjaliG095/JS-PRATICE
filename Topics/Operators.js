9b04d2248ea0dbf96bedd02214115d6fb5832bc3
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-06-28 18:20:13 +0530

    All Operators

---
+++ Topics/Operators.js
@@ -0,0 +1,55 @@
+ // Arithmetic Operators
+    let a = 10;
+    let b = 5;
+
+    console.log("Addition:", a + b);
+    console.log("Subtraction:", a - b);
+    console.log("Multiplication:", a * b);
+    console.log("Division:", a / b);
+    console.log("Modulus:", a % b);
+    console.log("Exponent:", a ** b);
+
+    // Increment & Decrement
+    let x = 5;
+    console.log(++x); // Pre-increment
+    console.log(--x); // Pre-decrement
+
+    // Assignment Operators
+    let c = 10;
+    c += 5;
+    console.log(c);
+
+    c -= 2;
+    console.log(c);
+
+    c *= 2;
+    console.log(c);
+
+    c /= 2;
+    console.log(c);
+
+    c %= 3;
+    console.log(c);
+
+    // Comparison Operators
+    let p = 20;
+    let q = 10;
+
+    console.log(p > q);
+    console.log(p < q);
+    console.log(p >= q);
+    console.log(p <= q);
+    console.log(p == q);
+    console.log(p != q);
+    console.log(p === q);
+    console.log(p !== q);
+
+    // Ternary Operator
+    let age = 18;
+    let result = age >= 18 ? "Adult" : "Minor";
+    console.log(result);
+
+    // Type Operator
+    console.log(typeof age);
+    console.log(typeof "JavaScript");
+    console.log(typeof true);
