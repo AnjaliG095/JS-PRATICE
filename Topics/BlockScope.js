b0cc324c1c62af267605d98eb6b5db79b3e0bbca
AnjaliG095 <rum2425604@ramauniversity.ac.in>
2026-07-08 21:09:43 +0530

    Block Scopes in js

---
+++ Topics/BlockScope.js
@@ -0,0 +1,10 @@
+//Variables declared with let and const are only accessible inside the block ({}).
+{
+    let a = 10;
+    let b = 20 ;
+    console.log(a);
+    console.log(b);
+}
+//Error
+ console.log(a);
+    console.log(b);
