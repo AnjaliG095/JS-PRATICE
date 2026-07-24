fdd547bd7d044c9340d3cc8031b74d74d9c4954f
Anjali Gurjar <rum2425604@ramauniversity.ac.in>
2026-07-24 18:14:07 +0530

    Changes

---
+++ DOM/Properpeties.js
@@ -0,0 +1,7 @@
+let para = document.getElementById("description");
+//Inner html conatins Tag
+console.log(para.innerHTML);
+//Inner text contains only visbile text  
+console.log(para.innerText);
+//Text Content contains full text hidden also
+console.log(para.textContent);
---
+++ DOM/QuerySelector.js
@@ -0,0 +1,14 @@
+document.getElementById("mainImg");
+document.getElementsByClassName("oldImg");
+document.getElementsByTagName("h1");
+document.querySelector(".boxlink");
+document.querySelectorAll("img");
+console.log(document.getElementById("mainImg"));
+
+console.log(document.getElementsByClassName("oldImg"));
+
+console.log(document.getElementsByTagName("h1"));
+
+console.log(document.querySelector(".boxLink"));
+
+console.log(document.querySelectorAll("img"));
